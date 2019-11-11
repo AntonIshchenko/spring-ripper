d12d978380875bf9708e7b64432404b10573aacd
Anton Ishchenko <anton_ishchenko@eppam.com>
2019-11-11 22:12:39 +0300

    Added annotation

--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -1,14 +1,18 @@
 package quoters;
 
 public class TerminatorQuoter implements Quoter {
+    @InjectRandomInt(min = 2, max = 7)
+    private int repeat;
 
-  private String message;
+    private String message;
 
-  public void setMessage(String message) {
-    this.message = message;
-  }
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
-  public void sayQuote() {
-    System.out.println("message = " + message);
-  }
+    public void sayQuote() {
+        for (int i = 0; i < repeat; i++) {
+            System.out.println("message = " + message);
+        }
+    }
 }
