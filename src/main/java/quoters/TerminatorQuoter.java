1b0dc9c1aeec4fda34c37b340ef1934f980c93d4
Anton Ishchenko <anton_ishchenko@eppam.com>
2019-11-11 22:41:10 +0300

    two phases implemented

--- src/main/java/quoters/TerminatorQuoter.java
+++ src/main/java/quoters/TerminatorQuoter.java
@@ -1,10 +1,24 @@
 package quoters;
 
+import org.springframework.context.annotation.CommonAnnotationBeanPostProcessor;
+
+import javax.annotation.PostConstruct;
+
 public class TerminatorQuoter implements Quoter {
+
     @InjectRandomInt(min = 2, max = 7)
     private int repeat;
-
     private String message;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Phase 2");
+        System.out.println(repeat);
+    }
+
+    public TerminatorQuoter() {
+        System.out.println("Phase 1");
+    }
 
     public void setMessage(String message) {
         this.message = message;
