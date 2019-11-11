31968fc0ba734ff63d81846d16b1d68ede89d689
Anton Ishchenko <anton_ishchenko@eppam.com>
2019-11-12 00:11:33 +0300

    implemented depricated annotation

--- src/main/java/quoters/DeprecatedClass.java
+++ src/main/java/quoters/DeprecatedClass.java
@@ -4,9 +4,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
-public @interface InjectRandomInt {
+public @interface DeprecatedClass {
 
-    int min();
+    Class newImpl();
 
-    int max();
 }
